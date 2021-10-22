ec1678bd453654f6cfce3cea317b6fce43423667
Orion Ford <39802627+DesignisOrion@users.noreply.github.com>
2021-10-22 16:03:38 -0400

    adding files

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+{/* Importing Image Picker library */}
+import * as ImagePicker from 'expo-image-picker';
+{/* Import Share Image library */}
+import * as Sharing from 'expo-sharing'; 
+{/*Importing the logo from the assets folder*/}
+import logo from './assets/floppy.png'; 
+
 
 export default function App() {
+
+  {/*Allowing selection of photo in app*/}
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/*Adding Image */}
+      <Image source={logo} style={{ width: 305, height: 159 }} /> 
+
+      {/*Adding styles to the text */}
+      <Text style={{color: '#888', fontSize: 18}}>To share a photo from your phone with a friend, just press the button below!</Text>
+
+      {/*Adding Button */}
+      <TouchableOpacity onPress={() => alert('Button is clicked!')} style={{ backgroundColor: 'green' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
